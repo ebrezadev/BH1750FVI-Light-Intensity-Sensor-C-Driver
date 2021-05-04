e5d10a385042b929ec1027b3d670be360d1d052a
Reza Ebrahimi <69025534+ebrezadev@users.noreply.github.com>
2021-05-03 20:00:25 -0700

    Update bh1750fvi_low_level.c

--- bh1750fvi_low_level.c
+++ bh1750fvi_low_level.c
@@ -1,36 +1,21 @@
 #include "bh1750fvi.h"
-#include <Wire.h>
 
-#define I2C_SPEED       400000        /*according to datasheet, bh1750fvi supports 400khz i2c speed*/
-
-/*transmits one byte of command to bh1750fvi*/
+/*transmits one byte of command to bh1750fvi. in case of bh1750 sensor, there's no internal register map. each 'write' to i2c is a 'command'*/
 void light_i2c_write_single(uint8_t device_address, uint8_t command)
 {
-  Wire.beginTransmission(device_address);
-  Wire.write(command);
-  Wire.endTransmission(device_address);
 }
 
-/*function to read two bytes of measured illuminance from bh1750fvi*/
+/*function to read two bytes of measured illuminance from bh1750fvi. note: there's no register address, just request two bytes of data from bh1750 (slave)*/
 void light_i2c_read(uint8_t device_address, uint8_t *data_array)
 {
-  Wire.requestFrom(device_address, 2, 0);
-  while (Wire.available())
-  {
-    *data_array = Wire.read();
-    data_array++;
-  }
 }
 
 /*function to initialize I2C peripheral in 100khz or 400khz*/
 void bh1750_I2C_init()
 {
-  Wire.begin();
-  Wire.setClock(I2C_SPEED);
 }
 
 /*function for delay in milliseconds*/
 void delay_function(uint32_t delay_milliseconds)
 {
-  delay(delay_milliseconds);
 }
